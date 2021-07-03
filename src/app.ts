31010f39111ef40700cf293f134790263f26b17b
Bruno Moreira <bruno@brunormoreira.com>
2021-07-03 18:50:17 -0300

    Add aux folders

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,9 @@
 import Koa from 'koa'
 import bodyParser from 'koa-bodyparser'
-import Router from 'koa-router'
+
+import router from './routes'
 
 const app = new Koa()
-const router = new Router()
-
-router.get('/', async (ctx, next) => {
-  ctx.body = {
-    message: 'Hello World',
-  }
-
-  await next()
-})
 
 app.use(bodyParser())
 
